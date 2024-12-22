7d7620a3f5d81e8fb27a8acee39185945203be64
Makoo <141389899+makuu12@users.noreply.github.com>
2024-12-22 11:03:30 +0800

    forms and assets

---
+++ DemoDB1/loginForm.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class loginForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Textpassword = New System.Windows.Forms.TextBox()
+        Me.Textusername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(469, 258)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(182, 116)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "&Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(210, 258)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(173, 122)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "&Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Textpassword
+        '
+        Me.Textpassword.Location = New System.Drawing.Point(285, 162)
+        Me.Textpassword.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Textpassword.Name = "Textpassword"
+        Me.Textpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.Textpassword.Size = New System.Drawing.Size(306, 26)
+        Me.Textpassword.TabIndex = 9
+        '
+        'Textusername
+        '
+        Me.Textusername.Location = New System.Drawing.Point(285, 71)
+        Me.Textusername.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Textusername.Name = "Textusername"
+        Me.Textusername.Size = New System.Drawing.Size(306, 26)
+        Me.Textusername.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(150, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(150, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Username:"
+        '
+        'loginForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Textpassword)
+        Me.Controls.Add(Me.Textusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "loginForm"
+        Me.Text = "loginForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Textpassword As TextBox
+    Friend WithEvents Textusername As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
