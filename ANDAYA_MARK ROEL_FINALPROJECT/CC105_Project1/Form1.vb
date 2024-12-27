f4c71fcbf2eea545355f26bb03a3a1eecdee3bd6
Makoo <141389899+makuu12@users.noreply.github.com>
2024-12-27 19:46:01 +0800

    form

---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub LoginToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginToolStripMenuItem.Click
+        Login.ShowDialog()
+    End Sub
+
+    Private Sub AdminToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AdminToolStripMenuItem.Click
+        Admin1.ShowDialog()
+    End Sub
+
+    Private Sub LogoutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LogoutToolStripMenuItem.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub PrintToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintToolStripMenuItem.Click
+
+        printform.ShowDialog()
+
+    End Sub
+
+    Private Sub LabExamToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        lab.ShowDialog()
+
+    End Sub
+End Class
