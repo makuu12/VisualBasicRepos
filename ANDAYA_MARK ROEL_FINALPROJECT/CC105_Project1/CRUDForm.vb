d7b596e5c2173a552ba9ce535d493baf0ccdfb37
Makoo <141389899+makuu12@users.noreply.github.com>
2024-12-27 19:45:48 +0800

    form

---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/CRUDForm.vb
@@ -0,0 +1,179 @@
+﻿
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class CRUDForm
+    Dim con1 As SqlClient.SqlConnection
+    Dim cmd As SqlClient.SqlCommand
+    Dim da1 As SqlClient.SqlDataAdapter
+    Dim sqladd As String
+    Dim dt As DataTable
+
+
+    Public Sub Binddata()
+        Dim sql As String = "SELECT * FROM member"
+        con1 = New SqlClient.SqlConnection
+        con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2aCRUD;User ID=sa;Password=1234;Pooling=False"
+        cmd = New SqlClient.SqlCommand(sql, con1)
+        da1 = New SqlClient.SqlDataAdapter()
+        da1.SelectCommand = cmd
+        dt = New DataTable
+        da1.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub Form1_Load(Sender As Object, e As EventArgs) Handles MyBase.Load
+        Binddata()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2aCRUD;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            Dim firstname1, lastname1, gender1 As String
+            firstname1 = TextBox1.Text.Trim()
+            lastname1 = TextBox2.Text.Trim()
+            gender1 = ComboBox1.SelectedItem
+            sqladd = "insert into member (first, last, gender) values ('" & firstname1 & "','" & lastname1 & "','" & gender1 & "')"
+            cmd = New SqlClient.SqlCommand(sqladd, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Successful inserted")
+            Binddata()
+            TextBox1.Clear()
+            TextBox2.Clear()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2aCRUD;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            Dim firstname1, lastname1, gender1, newid As String
+            newid = TextBox4.Text
+            firstname1 = TextBox1.Text
+            lastname1 = TextBox2.Text
+            gender1 = ComboBox1.SelectedItem
+            Dim sqlupdate = "UPDATE member SET " & "first = '" & firstname1 & "'," &
+"last = '" & lastname1 & "'," &
+                                                     "gender = '" & gender1 & "' WHERE id=" & TextBox4.Text
+
+            cmd = New SqlClient.SqlCommand(sqlupdate, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record updated")
+            Binddata()
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox4.Text = ""
+            ComboBox1.Text = ""
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("Are you sure you want to Delete this row", "Attention!", MessageBoxButtons.YesNo)
+        If Windows.Forms.DialogResult.Yes Then
+            Try
+                con1 = New SqlClient.SqlConnection
+                con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2aCRUD;User ID=sa;Password=1234;Pooling=False"
+                con1.Open()
+                Dim newid As String
+                newid = TextBox4.Text
+                Dim sqldelete As String = "DELETE FROM member WHERE id= " & TextBox4.Text
+                cmd = New SqlClient.SqlCommand(sqldelete, con1)
+                cmd.CommandType = CommandType.Text
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Record deleted")
+                Binddata()
+                TextBox1.Text = ""
+                TextBox2.Text = ""
+                TextBox4.Text = ""
+            Catch ex As Exception
+                MessageBox.Show(ex.Message.ToString)
+            End Try
+        End If
+        Binddata()
+    End Sub
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Dim current_row As Integer = DataGridView1.CurrentRow.Index
+        TextBox4.Text = DataGridView1(0, current_row).Value.ToString
+        TextBox1.Text = DataGridView1(1, current_row).Value.ToString
+        TextBox2.Text = DataGridView1(2, current_row).Value.ToString
+        ComboBox1.Text = DataGridView1(3, current_row).Value.ToString
+    End Sub
+
+    Private Sub SearchBox_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Dim sqlsearch As String = "SELECT * FROM member WHERE id ='" & TextBox3.Text & "'"
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2aCRUD;User ID=sa;Password=1234;Pooling=False"
+            cmd = New SqlClient.SqlCommand(sqlsearch, con1)
+            cmd.CommandType = CommandType.Text
+            da1 = New SqlClient.SqlDataAdapter()
+            da1.SelectCommand = cmd
+            dt = New DataTable
+            da1.Fill(dt)
+            If dt.Rows.Count > 0 Then
+                TextBox4.Text = dt.Rows(0)(0).ToString()
+                TextBox1.Text = dt.Rows(0)(1).ToString()
+                TextBox2.Text = dt.Rows(0)(2).ToString()
+                ComboBox1.Text = dt.Rows(0)(3).ToString()
+            Else
+                TextBox4.Clear()
+                TextBox1.Clear()
+                TextBox2.Clear()
+                ComboBox1.Text = ""
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
+
+
---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.AuthenticationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoginToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LogoutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RecordToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdminToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'AuthenticationToolStripMenuItem
+        '
+        Me.AuthenticationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoginToolStripMenuItem, Me.LogoutToolStripMenuItem})
+        Me.AuthenticationToolStripMenuItem.Name = "AuthenticationToolStripMenuItem"
+        Me.AuthenticationToolStripMenuItem.Size = New System.Drawing.Size(143, 29)
+        Me.AuthenticationToolStripMenuItem.Text = "Authentication"
+        '
+        'LoginToolStripMenuItem
+        '
+        Me.LoginToolStripMenuItem.Name = "LoginToolStripMenuItem"
+        Me.LoginToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.LoginToolStripMenuItem.Text = "login"
+        '
+        'LogoutToolStripMenuItem
+        '
+        Me.LogoutToolStripMenuItem.Enabled = False
+        Me.LogoutToolStripMenuItem.Name = "LogoutToolStripMenuItem"
+        Me.LogoutToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.LogoutToolStripMenuItem.Text = "logout"
+        '
+        'RecordToolStripMenuItem
+        '
+        Me.RecordToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AdminToolStripMenuItem, Me.PrintToolStripMenuItem})
+        Me.RecordToolStripMenuItem.Enabled = False
+        Me.RecordToolStripMenuItem.Name = "RecordToolStripMenuItem"
+        Me.RecordToolStripMenuItem.Size = New System.Drawing.Size(83, 29)
+        Me.RecordToolStripMenuItem.Text = "Record"
+        '
+        'AdminToolStripMenuItem
+        '
+        Me.AdminToolStripMenuItem.Name = "AdminToolStripMenuItem"
+        Me.AdminToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.AdminToolStripMenuItem.Text = "admin"
+        '
+        'PrintToolStripMenuItem
+        '
+        Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.PrintToolStripMenuItem.Text = "Print"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AuthenticationToolStripMenuItem, Me.RecordToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(959, 33)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(218, Byte), Integer), CType(CType(220, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(959, 679)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Employee Record"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AuthenticationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoginToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogoutToolStripMenuItem As ToolStripMenuItem
+    Public WithEvents RecordToolStripMenuItem As ToolStripMenuItem
+    Public WithEvents AdminToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+End Class
