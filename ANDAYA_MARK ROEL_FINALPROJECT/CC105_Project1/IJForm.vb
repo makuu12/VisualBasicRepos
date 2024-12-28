58be28c292f5d47788a88ea734d085417fd59bbb
Makoo <141389899+makuu12@users.noreply.github.com>
2024-12-28 23:44:59 +0800

    form

---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/IJForm.vb
@@ -0,0 +1,207 @@
+﻿Imports System.Collections.Specialized.BitVector32
+
+Public Class IJForm
+    Dim con1 As SqlClient.SqlConnection
+    Dim cmd As SqlClient.SqlCommand
+    Dim da1 As SqlClient.SqlDataAdapter
+    Dim sqladd As String
+    Dim dt As DataTable
+    Dim id As Int64
+    Dim firstname1, lastname1, course1, section1 As String
+
+    Private Sub LinkingTablesForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con1 = New SqlClient.SqlConnection
+        con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+        'Dim viewrecords = "SELECT id, firstname, lastname, course_desc, sectionname FROM viewrec" VIEW
+        Dim viewrecords = "Select DISTINCT id, firstname, lastname, courses_table.course_desc, section_table.sectionname
+FROM students_table INNER JOIN courses_table 
+ON students_table.courseid = courses_table.courseid 
+INNER JOIN section_table
+ON students_table.sectionid = section_table.sectionid"
+        cmd = New SqlClient.SqlCommand(viewrecords, con1)
+        cmd.CommandType = CommandType.Text
+        da1 = New SqlClient.SqlDataAdapter()
+        da1.SelectCommand = cmd
+        dt = New DataTable
+        da1.Fill(dt)
+        DataGridView1.DataSource = dt
+        loadsections()
+        loadcourses()
+    End Sub
+
+
+
+    Public Sub loadsections()
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            Dim cmd1 As New SqlClient.SqlCommand("Select DISTINCT students_table.courseid, courses_table.course_desc FROM students_table INNER JOIN courses_table ON students_table.courseid = courses_table.courseid", con1)
+            cmd1.CommandType = CommandType.Text
+            Dim da2 As New SqlClient.SqlDataAdapter
+            da2.SelectCommand = cmd1
+            Dim dt1 As New DataTable
+            If da2.Fill(dt1) Then
+                ComboBox1.DataSource = dt1
+                ComboBox1.DisplayMember = "course_desc"
+                ComboBox1.ValueMember = "courseid"
+                ComboBox1.Text = "Select Course"
+            Else
+                ComboBox1.DataSource = Nothing
+                ComboBox1.Text = "Select Course"
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.StackTrace)
+        End Try
+    End Sub
+    Public Sub loadcourses()
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            Dim cmd1 As New SqlClient.SqlCommand("SELECT DISTINCT students_table.sectionid, section_table.sectionname FROM students_table INNER JOIN section_table ON students_table.sectionid = section_table.sectionid", con1)
+            cmd1.CommandType = CommandType.Text
+            Dim da2 As New SqlClient.SqlDataAdapter
+            da2.SelectCommand = cmd1
+            Dim dt1 As New DataTable
+            If da2.Fill(dt1) Then
+                ComboBox2.DataSource = dt1
+                ComboBox2.DisplayMember = "sectionname"
+                ComboBox2.ValueMember = "sectionid"
+                ComboBox2.Text = "Select Section"
+            Else
+                ComboBox2.DataSource = Nothing
+                ComboBox2.Text = "Select Section"
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.StackTrace)
+        End Try
+    End Sub
+
+
+
+    Public Sub sponge()
+        con1 = New SqlClient.SqlConnection
+        con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+        'Dim viewrecords = "SELECT id, firstname, lastname, course_desc, sectionname FROM viewrec" VIEW
+        Dim viewrecords = "
+Select DISTINCT id, firstname, lastname, courses_table.course_desc, section_table.sectionname
+FROM students_table 
+INNER JOIN courses_table 
+ON students_table.courseid = courses_table.courseid 
+INNER JOIN section_table
+ON students_table.sectionid = section_table.sectionid"
+        cmd = New SqlClient.SqlCommand(viewrecords, con1)
+        cmd.CommandType = CommandType.Text
+        da1 = New SqlClient.SqlDataAdapter()
+        da1.SelectCommand = cmd
+        dt = New DataTable
+        da1.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            firstname1 = TextBox1.Text.Trim()
+            lastname1 = TextBox2.Text.Trim()
+            course1 = ComboBox1.SelectedValue
+            section1 = ComboBox2.SelectedValue
+            sqladd = "insert into students_table (firstname, lastname, courseid, sectionid) VALUES ('" & firstname1 & "','" & lastname1 & "','" & course1 & "', '" & section1 & "')"
+            cmd = New SqlClient.SqlCommand(sqladd, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Successful inserted")
+            sponge()
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            loadsections()
+            loadcourses()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            firstname1 = TextBox1.Text
+            lastname1 = TextBox2.Text
+            course1 = ComboBox1.SelectedValue
+            section1 = ComboBox2.SelectedValue
+            id = TextBox4.Text
+            Dim updaterow = "UPDATE updaterec SET firstname = '" & firstname1 & "', lastname = '" & lastname1 & "', courseid = '" & course1 & "', sectionid = '" & section1 & "'WHERE id = " & id & ""
+            cmd = New SqlClient.SqlCommand(updaterow, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record updated")
+            sponge()
+
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox4.Text = ""
+            loadsections()
+            loadcourses()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            Dim sqldelete As String = "SELECT * FROM Viewrec DELETE FROM students_table WHERE id = " & TextBox4.Text
+            cmd = New SqlClient.SqlCommand(sqldelete, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Record deleted")
+            sponge()
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox4.Text = ""
+            loadsections()
+            loadcourses()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            Dim sqlsearch As String = "SELECT id, firstname, lastname, course_desc, sectionname FROM viewrec WHERE id='" & TextBox3.Text & "'"
+            con1 = New SqlClient.SqlConnection
+            con1.ConnectionString = "Data Source=DESKTOP-G6V20H0\SQL_SERVER;Initial Catalog=sfit2a_relational;User ID=sa;Password=1234;Pooling=False"
+            con1.Open()
+            cmd = New SqlClient.SqlCommand(sqlsearch, con1)
+            cmd.CommandType = CommandType.Text
+            cmd.ExecuteNonQuery()
+            da1 = New SqlClient.SqlDataAdapter()
+            da1.SelectCommand = cmd
+            dt = New DataTable
+            da1.Fill(dt)
+            If dt.Rows.Count > 0 Then
+                TextBox4.Text = dt.Rows(0)(0).ToString()
+                TextBox1.Text = dt.Rows(0)(1).ToString()
+                TextBox2.Text = dt.Rows(0)(2).ToString()
+                ComboBox1.Text = dt.Rows(0)(3).ToString()
+                ComboBox2.Text = dt.Rows(0)(4).ToString()
+            Else
+                TextBox4.Clear()
+                TextBox1.Clear()
+                TextBox2.Clear()
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message.ToString)
+        End Try
+    End Sub
+End Class
+
---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/lab.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class lab
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(94, 94)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter Price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(141, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Prices"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(69, 384)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(173, 36)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Total Price: "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(213, 95)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(394, 26)
+        Me.TextBox1.TabIndex = 3
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(213, 168)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(394, 204)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(635, 94)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(635, 168)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 42)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Total"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(635, 231)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lab
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "lab"
+        Me.Text = "lab"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
