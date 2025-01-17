2be74dde81e0e4fda08ec5428e1fb54670dbc08e
Makoo <141389899+makuu12@users.noreply.github.com>
2025-01-17 18:17:07 +0800

    printform

---
+++ ANDAYA_MARK ROEL_FINALPROJECT/CC105_Project1/printform.vb
@@ -0,0 +1,13 @@
+﻿Public Class printform
+    Private Sub printform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'Sfit2a_andayaDataSet2.admin' table. You can move, or remove it, as needed.
+        Me.AdminTableAdapter.Fill(Me.Sfit2a_andayaDataSet2.admin)
+
+        Me.ReportViewer1.RefreshReport()
+
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+End Class
